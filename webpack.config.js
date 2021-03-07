44a0fb439660aafb59bb262d56ac8665de1955c7
Andrew Kwong <andy.kwong@me.com>
2021-03-06 19:39:36 -0800

    first commit, skeleton app

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+const BUILD_DIR = path.resolve(__dirname, 'public'); // where we will place our bundled file
+const APP_DIR = path.resolve(__dirname, 'client'); // where the pre-transpiled components live
+
+module.exports = {
+  mode: 'development',
+  entry: `${APP_DIR}/index.jsx`, // need to reference the point where we call reactdom.render
+  output: {
+    path: BUILD_DIR, // where we want to put bundle.js usually wherever index.html is
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        include: APP_DIR,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react', '@babel/preset-env'],
+          plugins: ['@babel/plugin-transform-runtime'],
+        },
+      },
+    ],
+  },
+  watch: true,
+  resolve: { extensions: ['.js', '.jsx'] },
+};
