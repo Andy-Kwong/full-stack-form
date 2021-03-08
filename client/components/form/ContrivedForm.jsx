9f534fcb4ba35beb5af8b124f18b9f072489487f
Andrew Kwong <andy.kwong@me.com>
2021-03-07 22:07:58 -0800

    add mongodb and express routes

--- client/components/form/ContrivedForm.jsx
+++ client/components/form/ContrivedForm.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 import { colors } from '../utilities/utilities';
 import ErrorMessage from './ErrorMessage';
+import BaseModal from '../modal/BaseModal';
 
 const Wrapper = styled.div`
   width: 100vw;
@@ -17,10 +19,12 @@ const Title = styled.h1`
 `;
 
 const Form = styled.form`
+  position: relative;
   width: 50%;
   max-width: 450px;
   display: flex;
   flex-direction: column;
+  z-index: -1;
 `;
 
 const Label = styled.label`
@@ -70,8 +74,14 @@ export default function ContrivedForm() {
   const handleSubmit = (e) => {
     e.preventDefault();
     setFirstSubmit(false);
+    if (toDelete && fields.email) {
+      axios.delete('/api/user/', { data: { email: fields.email } })
+        .then(data => console.log(data));
+    }
     if (!fields.name || !fields.email || !fields.message) return;
     // make post/put req
+    axios.post('/api/user/', fields)
+      .then(data => console.log(data));
     console.log(fields);
     clearForm();
   };
